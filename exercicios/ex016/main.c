59664dc7efac9b093deaca9ed2e6471aa3fd2de1
renannrocha <renanrocha8897@gmail.com>
2023-04-09 14:32:28 -0300

    ex016

---
+++ exercicios/ex016/main.c
@@ -0,0 +1,44 @@
+/**<
+16. Escreva um programa que leia 10 números e:
+    a. Escreva o menor valor lido e o maior valor lido
+    b. Some-os.
+    c. Imprima sua média.
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int soma = 0;
+    int vet[10];
+    for(int i = 0; i < 9; i++){
+        printf("Digite um numero: ");
+        scanf("%d", &vet[i]);
+        soma = soma + vet[i];
+    }
+
+    int maior = vet[0];
+    for (int i = 0; i < 9; i++){
+        if (vet[i] > maior){
+            maior = vet[i];
+        }
+    }
+
+    int menor = vet[0];
+    for (int i = 0; i < 9; i++){
+        if (vet[i] < menor){
+            menor = vet[i];
+        }
+    }
+
+    double media = (double)soma / 10;
+
+    printf("maior valor = %d\n", maior);
+    printf("menor valor = %d\n", menor);
+    printf("soma = %d\n", soma);
+    printf("media = %.2lf\n", media);
+
+    return 0;
+}
