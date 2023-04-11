b360a2d8ec6a641aa9ab880d8f6eaf0e5b6f22e5
Renan Rocha <111977696+renannrocha@users.noreply.github.com>
2023-04-11 00:13:20 -0300

    Update main.c

--- exercicios/ex015/main.c
+++ exercicios/ex015/main.c
@@ -1,6 +1,11 @@
+// aluno : Marcio Renan Rocha da Silva / matricula : 202208950035
+
 #include <stdio.h>
+#include <locale.h>
+
 int main()
 {
+    setlocale(LC_ALL,"Portuguese");
     int x, y, z;
 
     printf("Digite um numero: ");
@@ -18,6 +23,16 @@ int main()
     }
     else {
         printf("Maior numero = %d\n", z);
+    }
+    
+    if (x < y && x < z) {
+       printf("Menor numero = %d\n", x);
+    }
+    else if (y < x && y < z){
+        printf("Menor numero = %d\n", y); 
+    }
+    else {
+         printf("Menor numero = %d\n", z);
     }
 
     return 0;
