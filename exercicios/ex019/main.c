e10767bacade0c20e650e62cacfe214b73835c32
renannrocha <renanrocha8897@gmail.com>
2023-04-11 14:54:22 -0300

    ajust ex019

--- exercicios/ex019/main.c
+++ exercicios/ex019/main.c
@@ -1,8 +1,4 @@
-/**<
-19. Faça um programa que peça ao usuário um número entre 12 e 20. Se a pessoa digitar um
-número diferente, mostrar a mensagem "entrada inválida" e solicitar o número novamente. Se
-digitar correto mostrar o número digitado.
-*/
+// aluno : Marcio Renan / matricula: 202208950035
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +8,6 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
     int numero;
-    char resp;
     do{
         printf("\nDigite um numero entre 12 e 20 : ");
         scanf("%d", &numero);
@@ -23,7 +18,6 @@ int main()
         else{
             printf("\no numero digitado esta fora do intervalo !\n");
         }
-
-    }while(numero < 12 || numero > 20 );
+    }while(numero < 12 || numero > 20);
     return 0;
 }
